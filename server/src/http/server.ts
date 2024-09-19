96d7fe9c166ce5a0ec333668d812a9404fdaae82
antonysantos71 <ribeiroantony02.56@gmail.com>
2024-09-19 10:44:18 -0300

    started frontend

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -13,18 +13,16 @@ import fastifyCors from "@fastify/cors";
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-  origin: '*',
-})
+  origin: "*",
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-
-app.register(createGoalRoute)
-app.register(createCompletionRoute)
-app.register(getPendingGoalsRoute)
-app.register(getWeekSummaryRoute)
-
+app.register(createGoalRoute);
+app.register(createCompletionRoute);
+app.register(getPendingGoalsRoute);
+app.register(getWeekSummaryRoute);
 
 app
   .listen({
